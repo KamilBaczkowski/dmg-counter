cb3228ae419f5ea23b9e88e2d48e6b08e03f1e96
Kavelach <kavelach@kavela.ch>
2023-04-15 16:30:56 +0200

    Change the algorithm so it is more efficient

--- src/probabilities.rs
+++ src/probabilities.rs
@@ -1,36 +1,52 @@
-use std::collections::HashMap;
+pub fn probability(dice_rolls: usize, dice_sides: usize, desired_outcome: usize) -> f32 {
+    let total_outcomes = (dice_sides as f32).powi(dice_rolls as i32);
+    let successful_outcomes = count_combinations(dice_rolls, dice_sides, desired_outcome) as f32;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
-pub enum RollsError {
-    InvalidInput
+    successful_outcomes / total_outcomes
 }
 
-// Returns a set of (dmg, roll_count) given a number of rolls of a n sided die.
-pub fn possible_roll_results(rolls: usize, sides: usize) -> Result<HashMap<usize, usize>, RollsError> {
-    if rolls < 1 || sides < 1 {
-        return Err(RollsError::InvalidInput);
+fn count_combinations(dice_rolls: usize, dice_sides: usize, desired_outcome: usize) -> usize {
+    // Track the highest possible number by rolling all highest.
+    let max = dice_rolls * dice_sides;
+    count_combinations_inner(dice_rolls, dice_sides, desired_outcome, max)
+}
+
+fn count_combinations_inner(
+    dice_left: usize,
+    dice_sides: usize,
+    desired_outcome: usize,
+    current_max: usize,
+) -> usize {
+    if dice_left == 1 {
+        if desired_outcome >= 1 && desired_outcome <= dice_sides { return 1; } else { return 0; }
     }
 
-    let combinations: usize = sides.pow(rolls as u32);
-    let mut possible_rolls = vec![vec![0; rolls]; combinations];
-
-    let mut period = combinations;
-    for i in 0..rolls {
-        period /= sides;
-        for (j, roll) in possible_rolls.iter_mut().enumerate() {
-            roll[i] = (j/period) % sides + 1;
-        }
+    if desired_outcome > current_max {
+        // If we roll max values, we won't match the target sum anyways, so lets bail.
+        return 0;
     }
 
-    let mut possible_sums = HashMap::new();
-    for roll in possible_rolls {
-        let iter = roll.into_iter();
-        let sum = iter.sum::<usize>();
-        let current: usize = *possible_sums.get(&sum).unwrap_or(&0);
-        possible_sums.insert(sum, current + 1);
+    if desired_outcome == current_max {
+        // There is only one possible combination left (rolling only max values).
+        return 1;
     }
-    Ok(possible_sums)
+
+    if desired_outcome < dice_left {
+        // There is no way to roll the required amount, since even rolling only 1s will result in overflow.
+        return 0;
+    }
+
+    let iter_max = if dice_sides > desired_outcome { desired_outcome - 1 } else { dice_sides };
+    let mut count = 0;
+    for current_value in 1..=iter_max {
+        count += count_combinations_inner(
+            dice_left - 1,
+            dice_sides,
+            desired_outcome - current_value,
+            current_max - dice_sides,
+        );
+    }
+    count
 }
 
 #[cfg(test)]
@@ -39,28 +55,61 @@ mod tests {
 
     #[test]
     fn test_possible_roll_results_valid_with_one_die() {
-        let mut result = HashMap::new();
-        result.insert(1, 1);
-        assert_eq!(possible_roll_results(1, 1), Ok(result));
-        let mut result = HashMap::new();
-        result.insert(1, 1);
-        result.insert(2, 1);
-        assert_eq!(possible_roll_results(1, 2), Ok(result));
-        let mut result = HashMap::new();
-        result.insert(1, 1);
-        result.insert(2, 1);
-        result.insert(3, 1);
-        assert_eq!(possible_roll_results(1, 3), Ok(result));
-        let mut result = HashMap::new();
-        result.insert(1, 1);
-        result.insert(2, 1);
-        result.insert(3, 1);
-        result.insert(4, 1);
-        assert_eq!(possible_roll_results(1, 4), Ok(result));
+        assert_eq!(count_combinations(1, 1, 1), 1);
+
+        assert_eq!(count_combinations(1, 2, 1), 1);
+        assert_eq!(count_combinations(1, 2, 2), 1);
+
+        assert_eq!(count_combinations(1, 3, 1), 1);
+        assert_eq!(count_combinations(1, 3, 2), 1);
+        assert_eq!(count_combinations(1, 3, 3), 1);
+
+        assert_eq!(count_combinations(1, 4, 1), 1);
+        assert_eq!(count_combinations(1, 4, 2), 1);
+        assert_eq!(count_combinations(1, 4, 3), 1);
+        assert_eq!(count_combinations(1, 4, 4), 1);
     }
 
     #[test]
-    fn test_possible_roll_results_rejects_invalid_inputs() {
+    fn test_possible_roll_results_valid_with_two_dies() {
+        assert_eq!(count_combinations(2, 1, 2), 1);
 
+        assert_eq!(count_combinations(2, 2, 2), 1);
+        assert_eq!(count_combinations(2, 2, 3), 2);
+        assert_eq!(count_combinations(2, 2, 4), 1);
+
+        assert_eq!(count_combinations(2, 3, 2), 1);
+        assert_eq!(count_combinations(2, 3, 3), 2);
+        assert_eq!(count_combinations(2, 3, 4), 3);
+        assert_eq!(count_combinations(2, 3, 5), 2);
+        assert_eq!(count_combinations(2, 3, 6), 1);
+
+        assert_eq!(count_combinations(2, 4, 2), 1);
+        assert_eq!(count_combinations(2, 4, 3), 2);
+        assert_eq!(count_combinations(2, 4, 4), 3);
+        assert_eq!(count_combinations(2, 4, 5), 4);
+        assert_eq!(count_combinations(2, 4, 6), 3);
+        assert_eq!(count_combinations(2, 4, 7), 2);
+        assert_eq!(count_combinations(2, 4, 8), 1);
+    }
+
+    #[test]
+    fn test_possible_roll_results_valid_with_more_dies() {
+        assert_eq!(count_combinations(3, 3, 3), 1);
+        assert_eq!(count_combinations(3, 3, 4), 3);
+        assert_eq!(count_combinations(3, 3, 5), 6);
+        assert_eq!(count_combinations(3, 3, 6), 7);
+        assert_eq!(count_combinations(3, 3, 7), 6);
+        assert_eq!(count_combinations(3, 3, 8), 3);
+        assert_eq!(count_combinations(3, 3, 9), 1);
+
+        // Now it becomes infeasible to test out everything.
+        assert_eq!(count_combinations(3, 4, 3), 1);
+        assert_eq!(count_combinations(3, 4, 7), 12);
+        assert_eq!(count_combinations(3, 4, 8), 12);
+        assert_eq!(count_combinations(3, 4, 12), 1);
+        // 8^9/156 is around that, 156 is from
+        // https://www.wolframalpha.com/input?i=9+dice+with+8+sides+that+result+in+55
+        assert_eq!(863109, count_combinations(9, 8, 55));
     }
 }
--- src/weapon.rs
+++ src/weapon.rs
@@ -1,4 +1,4 @@
-use crate::{probabilities::possible_roll_results};
+use crate::{probabilities::{probability}};
 
 #[derive(Debug)]
 #[derive(PartialEq)]
@@ -9,34 +9,23 @@ pub struct Weapon {
 }
 
 impl Weapon {
-    // fn min_damage(&self) -> usize {
-    //     self.dice_rolls * 1 + self.modifier
-    // }
+    fn min_damage(&self) -> usize {
+        self.dice_rolls + self.modifier
+    }
 
     // fn avg_damage(&self) -> usize {
     //     (self.min_damage() + self.max_damage()) / 2
     // }
 
-    // fn max_damage(&self) -> usize {
-    //     self.dice_rolls * self.dice_sides + self.modifier
-    // }
+    fn max_damage(&self) -> usize {
+        self.dice_rolls * self.dice_sides + self.modifier
+    }
 
     pub fn damage_probabilities(&self) -> Vec<(usize, f32)> {
-        let possible_dmg = possible_roll_results(self.dice_rolls, self.dice_sides).unwrap();
-
-        let total_roll_count: usize = self.dice_sides.pow(self.dice_rolls as u32);
-        let mut probabilities: Vec<(usize, f32)> = possible_dmg
-            .iter()
-            .map(|(dmg, count)| {
-                (dmg + self.modifier, *count as f32 / total_roll_count as f32)
-            })
-            .collect();
-
-        probabilities.sort_by(|(dmg_a, _), (dmg_b, _)| {
-            dmg_a.cmp(dmg_b)
-        });
-
-        probabilities
+        (self.min_damage()..=self.max_damage())
+            .map(|dmg| {
+                (dmg, probability(self.dice_rolls, self.dice_sides, dmg))
+            }).collect()
     }
 }
 
